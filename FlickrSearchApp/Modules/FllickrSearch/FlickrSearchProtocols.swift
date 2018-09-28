8ba2167ea91aeedd9c08ac7dabc9c5c5cdcd67d6
Suhit Patil <suhit.patil@gmail.com>
2018-09-28 19:31:12 +0530

    Added FlickrSearchModule classes

---
+++ FlickrSearchApp/Modules/FllickrSearch/FlickrSearchProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  FlickrSearchProtocols.swift
+//  FlickrSearchApp
+//
+//  Created by Suhit Patil on 28/09/18.
+//  Copyright © 2018 Suhit Patil. All rights reserved.
+//
+
+import Foundation
