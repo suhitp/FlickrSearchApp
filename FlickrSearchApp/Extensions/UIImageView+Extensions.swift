81a65c0b589ab52c9775f3720890c062b930c1f4
Suhit Patil <suhit.patil@gmail.com>
2020-10-24 10:27:31 +0530

    image duplication fix (#2)
    image duplication fixes when fast scrolling

--- FlickrSearchApp/Extensions/UIImageView+Extensions.swift
+++ FlickrSearchApp/Extensions/UIImageView+Extensions.swift
@@ -8,35 +8,75 @@
 
 import UIKit
 
+struct AssociatedKeys {
+    static var imageUrlKey: UInt64 = 0
+}
 
 extension UIImageView {
     
-    /*!
-     * @description: Load Image into imageView from the given imageUrl using ImageDownloader
-     * class
-     * @parameters: takes imageURL, placeholderImage and indexPath as params
-     */
-    func loadImage(with imageURL: URL, placeholder: UIImage? = UIImage(color: .black), size: CGSize, indexPath: IndexPath?) {
-        self.image = placeholder
+    private(set) var downloadImageURL: URL? {
+        get {
+            guard let value = objc_getAssociatedObject(self, &AssociatedKeys.imageUrlKey) as? URL else {
+                return nil
+            }
+            return value
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &AssociatedKeys.imageUrlKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    
+    /// load image from remote url
+    /// - Parameters:
+    ///   - imageURL: is the `URL` which points to Https url
+    ///   - placeholder: `UIImage` placeholder
+    ///   - size: image container size
+    func loadImage(
+        with imageURL: URL,
+        placeholder: UIImage? = UIImage(color: .black),
+        size: CGSize
+    ) {
+        downloadImageURL = imageURL
+        image = placeholder
         ImageDownloader.shared.downloadImage(
             withURL: imageURL,
             size: size,
-            indexPath: indexPath,
-            completion: { [weak self] (image: UIImage?, resultIndexPath: IndexPath?, url: URL, error: Error?) in
-                if let self = self, let kIndexPath = resultIndexPath, indexPath == kIndexPath, imageURL.absoluteString == url.absoluteString {
-                    DispatchQueue.main.async {
-                        if let downloadedImage = image {
-                            self.fadeTransition(with: downloadedImage)
+            completion: { [weak self] (image, isCached, url, error) in
+                guard let self = self, let downloadURL = self.downloadImageURL, imageURL == downloadURL else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let downloadedImage = image, let isCached = isCached {
+                        if isCached {
+                            self.image = downloadedImage
+                        } else {
+                            UIView.transition(with: self, duration: 0.5, options: .transitionCrossDissolve, animations: {
+                                self.image = downloadedImage
+                            }, completion: nil)
                         }
+                    } else {
+                        self.image = placeholder
                     }
                 }
-        })
+            }
+        )
     }
     
     //MARK: Fade transition Animation
-    func fadeTransition(with image: UIImage?, duration: TimeInterval = 0.5, completion: ((Bool) -> Void)? = nil) {
-        UIView.transition(with: self, duration: duration, options: .transitionCrossDissolve, animations: {
-            self.image = image
-        }, completion: completion)
+    func fadeTransition(
+        with image: UIImage?,
+        duration: TimeInterval = 0.5,
+        completion: ((Bool) -> Void)? = nil
+    ) {
+        UIView.transition(
+            with: self,
+            duration: duration,
+            options: .transitionCrossDissolve,
+            animations: {
+                self.image = image
+            },
+            completion: completion
+        )
     }
 }
